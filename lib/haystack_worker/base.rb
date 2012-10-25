30d30354442405897fa652ddb1da977659e62a0f
Chris Patuzzo <chris@patuzzo.co.uk>
2012-10-25 19:03:59 +0100

    Coercion and argument validation

--- lib/haystack_worker/base.rb
+++ lib/haystack_worker/base.rb
@@ -3,7 +3,17 @@ require 'benchmark'
 class HaystackWorker
   extend Benchmark
 
-  def self.work(ranges)
+  def self.work(*ranges)
+    validate(ranges)
     _work(ranges.map { |r| [r.min, r.max] })
+  end
+
+  private
+  def self.validate(ranges)
+    ranges.flatten!
+
+    message = 'Please specify 26 ranges as an argument.'
+    raise ArgumentError.new(message) unless ranges.count == 26
+    raise TypeError.new(message) unless ranges.all? { |r| r.is_a?(Range) }
   end
 end
--- spec/haystack_worker/base_spec.rb
+++ spec/haystack_worker/base_spec.rb
@@ -15,6 +15,28 @@ describe HaystackWorker do
       search_space = [1..1] * 26;
       HaystackWorker.work(search_space).should be_nil
     end
+
+    it 'raises an argument error on a cardinality mismatch' do
+      search_space = [1..1] * 25
+      expect {
+        HaystackWorker.work(search_space)
+      }.to raise_error(ArgumentError, /26/)
+    end
+
+    it 'raises a type error unless an array of ranges is given' do
+      expect {
+        HaystackWorker.work(['a'] * 26)
+      }.to raise_error(TypeError, /26/)
+
+      expect {
+        HaystackWorker.work([123] * 26)
+      }.to raise_error(TypeError, /26/)
+    end
+
+    it 'coerces list style arguments into an array' do
+      search_space = [1..1] * 26
+      HaystackWorker.work(*search_space).should be_nil
+    end
   end
 
 end
